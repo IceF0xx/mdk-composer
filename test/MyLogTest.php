9589c916d4da015ed9e666f9fbf38e3b6452de3b
miko <desu@miko.team>
2021-03-17 10:57:03 +0300

    Test changed

--- test/MyLogTest.php
+++ test/MyLogTest.php
@@ -7,8 +7,10 @@ use mihajlov\MyLog;
 class MyLogTest extends TestCase
 {
     function testGoodLog() {
-        $this->expectOutputString("Test Output\n");
-        MyLog::log("Test Output");
+        $this->expectOutputString("Test\nOutput\n");
+        MyLog::log("Test");
+        MyLog::log("Output");
+
         MyLog::write();
     }
 }
