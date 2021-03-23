33d2313987d8c3492a4330d7968e9a15cc37c4bf
miko <desu@miko.team>
2021-03-23 16:19:14 +0300

    Tests

--- test/MyLogTest.php
+++ test/MyLogTest.php
@@ -6,11 +6,37 @@ use mihajlov\MyLog;
 
 class MyLogTest extends TestCase
 {
-    function testGoodLog() {
-        $this->expectOutputString("Test\nOutput\n");
-        MyLog::log("Test");
-        MyLog::log("Output");
+    function testLogFileContent() {
+        $expectedString = "TEST SOME BULLSHIT";
+        $logTime = time();
+        $logFileDir = __DIR__ . "/logs/$logTime.log";
+
+        MyLog::log($expectedString);
+        MyLog::write();
+
+        $logFileContent = file_get_contents($logFileDir);
+
+        $this->assertEquals($expectedString . "\r\n", $logFileContent);
+        $this->expectOutputString($expectedString);
+    }
+
+    function testCheckLogFileExist() {
+        MyLog::log(__DIR__);
+
+        $logsCounter = 0;
+
+        foreach (glob(__DIR__ . "/logs/*.log") as $filename) {
+            $logsCounter += 1;
+        }
+
+        MyLog::log($logsCounter);
 
         MyLog::write();
+
+        foreach (glob(__DIR__ . "/logs/*.log") as $filename) {
+            $logsCounter -= 1;
+        }
+
+        $this->assertEquals(0, $logsCounter);
     }
 }
